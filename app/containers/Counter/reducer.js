42637cc82496b2e869dca4fffeb9fbc95065a404
youpiwaza <masamune.code@gmail.com>
2018-06-18 15:57:30 +0200

    ADD: reducers & test

--- app/containers/Counter/reducer.js
+++ app/containers/Counter/reducer.js
@@ -6,19 +6,53 @@
 
 import { fromJS } from 'immutable';
 import {
-//  DEFAULT_ACTION,
+  DECREMENT,
+  INCREMENT,
+  SET,
 } from './constants';
 
-// Store the count integer
 const initialState = fromJS({
   count: 0,
 });
 
 function counterReducer(state = initialState, action) {
+  // Remplacer switch par génération automatique de reducers
+  // https://redux.js.org/recipes/reducing-boilerplate#reducers
+
   switch (action.type) {
+    case DECREMENT:
+      // return { ...state, count: initialState.count - 1 };
+      return decrement(state);
+
+    case INCREMENT:
+      return increment(state);
+
+    case SET:
+      return set(state, action.count);
+
     default:
       return state;
   }
+}
+
+
+function decrement(state) {
+  const decremented = state.get('count') - 1;
+  return state.set('count', decremented);
+}
+
+function increment(state) {
+  const incremented = state.get('count') + 1;
+  return state.set('count', incremented);
+}
+
+function set(state, count) {
+  const conversion = parseInt(count, 10);
+
+  if (isNaN(conversion)) {
+    return state;
+  }
+  return state.set('count', conversion);
 }
 
 export default counterReducer;
