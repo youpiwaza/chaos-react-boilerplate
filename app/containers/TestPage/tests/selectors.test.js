9bdae3d582f828e082a2fcb65d5ad2e914c7faae
youpiwaza <masamune.code@gmail.com>
2018-06-18 13:49:45 +0200

    ADD: generated container

---
+++ app/containers/TestPage/tests/selectors.test.js
@@ -0,0 +1,8 @@
+// import { fromJS } from 'immutable';
+// import { selectTestPageDomain } from '../selectors';
+
+describe('selectTestPageDomain', () => {
+  it('TODO / Expect to have unit tests specified', () => {
+    expect(true).toEqual(true);
+  });
+});
